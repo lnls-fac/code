68b36f478e10839577892e6ea021e7a57e937b69
afonsoharuo <afonso@fac8-linux>
2013-12-17 18:22:10 -0200

    Possibilita várias linhas em InteractivePlot

--- control/modules/InteractivePlot/test/test_PlotSharedMemory.py
+++ control/modules/InteractivePlot/test/test_PlotSharedMemory.py
@@ -15,6 +15,24 @@ class TestCommand(unittest.TestCase):
     def setUp(self):
         self.shm = PlotSharedMemory.PlotSharedMemory()
     
+    def test_text_updated_not_bool(self):
+        result = False
+        try:
+            self.shm.text_updated = 1
+        except TypeError:
+            result = True
+        self.assertTrue(result,
+                        'TypeError not raised')
+    
+    def test_text_updated_default(self):
+        self.assertFalse(self.shm.text_updated,
+                         'text_updated not False')
+     
+    def test_text_updated(self):
+        self.shm.text_updated = True
+        self.assertTrue(self.shm.text_updated,
+                        'text_updated not True')
+    
     def test_data_updated_not_bool(self):
         result = False
         try:
