f135810f32cb47ce6f1754f9b80438077e2ef622
ximenes <xresende@gmail.com>
2014-03-13 09:15:31 -0300

    corrigindo bug de lnls1_fittune2

--- MatlabMiddleLayer/Release/machine/LNLS1/StorageRing/scripts/lnls1_fittune2.m
+++ MatlabMiddleLayer/Release/machine/LNLS1/StorageRing/scripts/lnls1_fittune2.m
@@ -12,9 +12,9 @@ THERING = setcellstruct(THERING, 'FamName', QD, 'QD');
 THERING = setcellstruct(THERING, 'FamName', QFC, 'QFC');
 fittune2(newtunes, 'QF', 'QD');
 quads = findcells(THERING, 'K');
-K = getcellstruct(THERING, 'K', idx);
+K = getcellstruct(THERING, 'K', quads);
 THERING = THERING0;
-THERING = setcellstruct(THERING, 'K', quas, K);
+THERING = setcellstruct(THERING, 'K', quads, K);
 THERING = setcellstruct(THERING, 'PolynomB', quads, K, 1, 2);
 
 
