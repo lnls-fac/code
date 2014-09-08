ff3fe88a88f608c5d60c90d05d0aeab82b1eecac
Fernando Henrique de Sá <fernando@lnls210-linux.abtlus.org.br>
2014-09-08 18:15:48 -0300

    bug

--- python/JobManager/src/Global.py
+++ python/JobManager/src/Global.py
@@ -367,7 +367,7 @@ def createfile(name= None, data=None, stats = MyStats(), owner = None):
         raise ValueError('Name not specified')
     try:
         with open(name, mode='wb') as fh:
-            fh.write(data or b'')
+            fh.write(data or ''.encode('utf-8'))
     except (IOError, OSError) as err:
         print('Problem with output files:\n',err)
         return None
