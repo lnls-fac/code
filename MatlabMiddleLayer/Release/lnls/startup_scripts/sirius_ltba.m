22b8c513cebb6a5693c7945fc39e0751d9c82d8e
liu <liulin>
2014-09-26 16:52:46 -0300

    created LTBA v300 lattice

--- MatlabMiddleLayer/Release/lnls/startup_scripts/sirius_ltba.m
+++ MatlabMiddleLayer/Release/lnls/startup_scripts/sirius_ltba.m
@@ -7,8 +7,8 @@ function sirius_ltba(varargin)
 
 Disconnect = false;
 
-default_version_sr   = '_V500';
-default_version_ltba = '_V200';
+default_version_sr   = '_V02';
+default_version_ltba = '_V300';
 
 for i=length(varargin):-1:1
     if ischar(varargin{i})
@@ -31,11 +31,8 @@ end
 
 % inicializa estruturas do MML
 cdir = pwd;
-if any(strcmpi(computer, {'PCWIN', 'PCWIN64'}))
-    cd('C:\Arq\MatlabMiddleLayer\Release\mml\');
-else
-    cd('/home/fac_files/code/MatlabMiddleLayer/Release/mml/');
-end
+root_folder = lnls_get_root_folder();
+cd(fullfile(root_folder, 'code', 'MatlabMiddleLayer','Release','mml'))
 
 setpathsirius(['SIRIUS' default_version_sr], ['LTBA' default_version_ltba], 'sirius_link');
 cd(cdir);
@@ -44,11 +41,9 @@ clear cdir;
 if any(strcmpi(computer, {'PCWIN','PCWIN64'}))
     addpath(genpath('C:\Arq\MatlabMiddleLayer\Release\lnls\fac_scripts\sirius\lattice_errors'));
     addpath(fullfile('C:\Arq\MatlabMiddleLayer\Release', 'lnls', 'fac_scripts', 'tracy3'), '-begin');
-    %addpath(genpath('C:\Arq\MatlabMiddleLayer\Release\machine\LTBA_V200'));
 else
     addpath(genpath('/home/fac_files/code/MatlabMiddleLayer/Release/lnls/fac_scripts/sirius/lattice_errors'));
     addpath(genpath('/home/fac_files/code/MatlabMiddleLayer/Release/lnls/fac_scripts/tracy3'));
-    %addpath(genpath('/home/fac_files/code/MatlabMiddleLayer/Release/machine/LTBA_V200'));
 end
 
 return;
