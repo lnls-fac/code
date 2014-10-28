e71b2fbd0ac6fe61d57e6a290d6f4fa278a604d3
Fernando Henrique de Sá <fernando@lnls82-linux.abtlus.org.br>
2014-10-28 15:13:24 -0200

    Change in visualization of calc_respm_coupling.
    Now the script prints the number os skew quads used for the correction

--- MatlabMiddleLayer/Release/lnls/fac_scripts/tracy3/tracy3_da_ma_lt.m
+++ MatlabMiddleLayer/Release/lnls/fac_scripts/tracy3/tracy3_da_ma_lt.m
@@ -15,13 +15,13 @@ function tracy3_da_ma_lt(n_calls, the_ring)
 % primeira fase
 twi = calctwiss(the_ring);
 
-%params.emit0 = 2.7e-10;
-%params.E     = 3e9;
-%params.N     = 100e-3/864/1.601e-19*1.72e-6;
-%params.sigE  = 0.87e-3;
-%params.sigS  = 3.5e-3;
-%params.K     = 0.01;
-%accepRF      = 0.05;
+params.emit0 = 2.7e-10;
+params.E     = 3e9;
+params.N     = 100e-3/864/1.601e-19*1.72e-6;
+params.sigE  = 0.87e-3;
+params.sigS  = 3.5e-3;
+params.K     = 0.01;
+accepRF      = 0.05;
 
 %% storage ring 
 % params.emit0 = 2.05e-10;
@@ -42,13 +42,13 @@ twi = calctwiss(the_ring);
 % accepRF      = 0.0061;
 
 % booster E = 0.15 GeV
-params.emit0 = 170e-9;
-params.E     = 0.15e9;
-params.N     = 0.6e-3/1/1.601e-19*1.72e-6;
-params.sigE  = 0.5e-3;
-params.sigS  = 11.2e-3;
-params.K     = 0.0002;
-accepRF      = 0.033;
+% params.emit0 = 170e-9;
+% params.E     = 0.15e9;
+% params.N     = 0.6e-3/1/1.601e-19*1.72e-6;
+% params.sigE  = 0.5e-3;
+% params.sigS  = 11.2e-3;
+% params.K     = 0.0002;
+% accepRF      = 0.033;
 
 
 % parâmetros para a geração das figuras
@@ -67,11 +67,7 @@ yf = yi + scrsz(4)*(2/3);
 % if ~exist('var_plane','var')
 var_plane = 'y'; %determinaçao da abertura dinâmica por varreduda no plano y
 % end
-<<<<<<< HEAD
 path = '/home/fac_files/data/sirius/si/beam_dynamics/';
-=======
-path = '/home/fac_files/data/sirius/';
->>>>>>> 1f74ef319aa7347c5ab6df641cdf99ce1f9dc214
 
 cell_leg_text = cell(1,n_calls);
 pl = zeros(n_calls,3);
