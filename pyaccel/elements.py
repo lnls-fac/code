ca75ebc950078e131027273108721a6daef5f9ba
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-01-20 14:20:42 -0200

    Add some old changes to pyaccel and trackc++

--- pyaccel/elements.py
+++ pyaccel/elements.py
@@ -17,11 +17,13 @@ _r_temp = _p_rotation_matrix.from_address(0)
 _r_class = _r_temp.__class__
 del(_r_temp)
 
+pass_methods = _trackcpp.pm_dict
+
 
 class Vector(_t_class):
 
-    def __repr__(self):
-        return self._get_str('Vector(', ')')
+    #def __repr__(self):
+    #    return self._get_str('Vector(', ')')
 
     def __str__(self):
         return self._get_str()
@@ -42,8 +44,8 @@ class TVector(Vector):
 
 class RMatrix(list):
 
-    def __repr__(self):
-        return self._get_str('RMatrix(', ')')
+    #def __repr__(self):
+    #    return self._get_str('RMatrix(', ')')
 
     def __str__(self):
         return self._get_str()
@@ -81,11 +83,17 @@ def _get_rotation_matrix(pointer):
 
 
 class Element(_trackcpp.Element):
+
+    _attributes_to_print = [
+            'fam_name',
+            'length',
+            'pass_method',
+            'hmax',
+            'vmax'
+    ]
     
-    pass_methods = _trackcpp.pm_dict
     _array_names = ['t_in', 't_out']
     _matrix_names = ['r_in', 'r_out']
-    _is_initialized = False
 
     def __init__(self, fam_name, length=0.0):
         super().__init__(fam_name, length)
@@ -94,8 +102,11 @@ class Element(_trackcpp.Element):
         self.r_in = _get_rotation_matrix(self._r_in)
         self.r_out = _get_rotation_matrix(self._r_out)
 
+    #def __repr__(self):
+    #    return self.__str__()
+
     def __str__(self):
-        s = [''] # get a newline before first attribute
+        s = [] # get a newline before first attribute
         for name in self.__class__._attributes_to_print:
             if name in Element._array_names:
                 value = self.__getattr__(name)[:]
@@ -128,17 +139,31 @@ class Element(_trackcpp.Element):
         for i in range(len(pa),n): pa.append(0.0)
         for i in range(len(pb),n): pb.append(0.0)
         return pa, pb
+
+    def _get_pass_method(self):
+        return pass_methods[self._pass_method]
+
+    def _set_pass_method(self, value):
+        print('oi')
+        if isinstance(value, int):
+            if (0 <= value < len(pass_methods)):
+                self._pass_method = value
+            else:
+                raise ValueError('pass method out of range')
+        elif isinstance(value, str):
+            try:
+                index = pass_methods.index(value)
+            except:
+                raise ValueError('pass method not found')
+        else:
+            raise TypeError('pass method must be int or str')
+
+    pass_method = property(_get_pass_method, _set_pass_method)
             
 
 class Marker(Element):
 
-    _attributes_to_print = [
-                'fam_name',
-                'length',
-                'pass_method',
-                'hmax',
-                'vmax'
-        ]
+    _attributes_to_print = Element._attributes_to_print
     
     def __init__(self, fam_name):
         """Create a marker element.
@@ -163,13 +188,7 @@ class Bpm(Marker):
 
 class Drift(Element):
 
-    _attributes_to_print = [
-                'fam_name',
-                'length',
-                'pass_method',
-                'hmax',
-                'vmax'
-        ]
+    _attributes_to_print = Element._attributes_to_print
  
     def __init__(self, fam_name, length):
         """Create a drift element.
@@ -185,15 +204,8 @@ class Drift(Element):
 
 class Corrector(Element):
 
-    _attributes_to_print = [
-                'fam_name',
-                'length',
-                'pass_method',
-                'hmax',
-                'vmax',
-                'hkick',
-                'vkick'
-        ]
+    _attributes_to_print = Element._attributes_to_print + ['hkick',
+                                                           'vkick']
 
     def __init__(self, fam_name, hkick, vkick, length = 0.0):
         """Create a corrector element.
@@ -236,20 +248,13 @@ class VCorrector(Corrector):
 
 class Quadrupole(Element):
 
-    _attributes_to_print = [
-                'fam_name',
-                'length',
-                'nr_steps',
-                'polynom_a',
-                'polynom_b',
-                'pass_method',
-                'hmax',
-                'vmax',
-                'r_in',
-                'r_out',
-                't_in',
-                't_out'
-        ]
+    _attributes_to_print = Element._attributes_to_print + ['nr_steps',
+                                                           'polynom_a',
+                                                           'polynom_b',
+                                                           'r_in',
+                                                           'r_out',
+                                                           't_in',
+                                                           't_out']
     
     def __init__(self, fam_name, length, K, nr_steps=10):
         """Create a quadrupole element.
@@ -267,9 +272,13 @@ class Quadrupole(Element):
 
 class Sextupole(Element):
 
-    _attributes_to_print = ['fam_name', 'length', 'nr_steps',
-                            'polynom_a', 'polynom_b', 'pass_method',
-                            'hmax', 'vmax', 'r_in', 'r_out', 't_in', 't_out']
+    _attributes_to_print = Element._attributes_to_print + ['nr_steps',
+                                                           'polynom_a',
+                                                           'polynom_b',
+                                                           'r_in',
+                                                           'r_out',
+                                                           't_in',
+                                                           't_out']
     
     def __init__(self, fam_name, length, S, nr_steps=5):
         """Create a sextupole element.
@@ -286,7 +295,19 @@ class Sextupole(Element):
 
 class RBend(Element):
 
-    def __init__(self, 
+    _attributes_to_print = Element._attributes_to_print + ['angle',
+                                                           'angle_in',
+                                                           'angle_out',
+                                                           'gap',
+                                                           'fint_in',
+                                                           'fint_out',
+                                                           'polynom_a',
+                                                           'polynom_b',
+                                                           'r_in',
+                                                           'r_out',
+                                                           't_in',
+                                                           't_out']
+    def __init__(self,
                  fam_name, length, 
                  angle, angle_in=0.0, angle_out=0.0,
                  gap=0.0, fint_in=0.0, fint_out=0.0,
@@ -314,6 +335,9 @@ class RBend(Element):
 
 
 class RFCavity(Element):
+
+    _attributes_to_print = Element._attributes_to_print + ['frequency',
+                                                           'voltage']
 
     def __init__(self, fam_name, length, frequency, voltage):
         """Create an RF cavity element.
@@ -343,141 +367,3 @@ def _format_string_to_print_element(name, value):
 
 _trackcpp.CppDoubleVector.__repr__ = _list_repr
 _trackcpp.CppDoubleVector.__str__ = _list_str
-
-
-#class PassMethodError(Exception):
-#    pass
-#
-#
-#class LengthError(Exception):
-#    pass
-#
-#
-#class _CArray:
-#
-#    def __init__(self, array, size):
-#        self._array = array
-#        self._size = size
-#
-#    def __getitem__(self, k):
-#        idx = self._check_and_get_indices(k, self._size)
-#        if idx is None:
-#            r = _trackcpp.c_array_get(self._array, k%self._size)
-#        else:
-#            r = []
-#            for i in idx:
-#                r.append(_trackcpp.c_array_get(self._array, i))
-#        return r
-#
-#    def __setitem__(self, k, value):
-#        idx = self._check_and_get_indices(k, self._size)
-#        if idx is None:
-#            if not isinstance(value, int):
-#                raise TypeError('value must be int')
-#            _trackcpp.c_array_set(self._array, k%self._size, value)
-#        else:
-#            n = len(value)
-#            if not n == len(idx):
-#                raise LengthError('lengths must be equal')
-#            for t in zip(idx, range(n)):
-#                _trackcpp.c_array_set(self._array, t[0], value[t[1]])
-#
-#    def _check_and_get_indices(self, k, size):
-#        if isinstance(k, int):
-#            if not -size <= k < size:
-#                raise IndexError('index out of range')
-#            idx = None
-#        elif isinstance(k, slice):
-#            k_indices = k.indices(size)
-#            idx = range(*k_indices)
-#        elif isinstance(k, list) or isinstance(k, tuple):
-#            if not 0 <= min(k) and size <= max(k):
-#                raise IndexError('index out of range')
-#            idx = k
-#        else:
-#            raise TypeError('indices must be numbers, not ' + type(k))
-#
-#        return idx
-#
-#
-#class TVector(_CArray):
-#
-#    def __repr__(self):
-#        return str(self[:])
-#
-#    def __str__(self):
-#        return str(self[:])
-#
-#
-#class RMatrix(_CArray):
-#
-#    def __init__(self, array, size):
-#        if not isinstance(size, tuple):
-#            raise TypeError('size must be tuple')
-#        if not len(size) == 2:
-#            raise LengthError('matrix must have two sizes')
-#
-#        self._rows = size[0]
-#        self._cols = size[1]
-#        self._m_size = (self._rows, self._cols)
-#        super().__init__(array, self._rows*self._cols)
-#
-#    def __getitem__(self, k):
-#        self._check_matrix_indices(k)
-#        if isinstance(k[0], int) and isinstance(k[1], int):
-#            m_idx = self._rows*k[0] + k[1]
-#            return _trackcpp.c_array_get(self._array, m_idx)
-#        else:
-#            idx = []
-#            for z in zip(k, self._m_size):
-#                i_temp = self._check_and_get_indices(z[0], z[1])
-#                if i_temp is None:
-#                    idx.append([z[0]])
-#                else:
-#                    idx.append(i_temp)
-#            m = []
-#            for i in idx[0]:
-#                m.append([])
-#                for j in idx[1]:
-#                    m_idx = self._rows*i + j
-#                    m[-1].append(_trackcpp.c_array_get(self._array, m_idx))
-#            return m
-#
-#    def __setitem__(self, k, value):
-#        self._check_matrix_indices(k)
-#        if isinstance(k[0], int) and isinstance(k[1], int):
-#            if not isinstance(value, (int, float)):
-#                raise TypeError('value must be number')
-#            if (not -self._rows <= k[0] < self._rows and
-#                    not -self._cols <= k[1] < self._cols):
-#                raise IndexError('index out of range')
-#            m_idx = self._rows*(k[0]%self._rows) + (k[1]%self._cols)
-#            _trackcpp.c_array_set(self._array, m_idx, value)
-#        else:
-#            if not isinstance(value, (list, tuple)):
-#                raise TypeError('value must be matrix')
-#            for row in value:
-#                if not isinstance(row, (list, tuple)):
-#                    raise TypeError('value must be matrix')
-#            idx = []
-#            for z in zip(k, self._m_size):
-#                i_temp = self._check_and_get_indices(z[0], z[1])
-#                if i_temp is None:
-#                    idx.append([z[0]%z[1]])
-#                else:
-#                    idx.append(i_temp)
-#            if not len(idx[0]) == len(value):
-#                raise LengthError('wrong number of rows')
-#            if not len(idx[1]) == len(value[0]):
-#                raise LengthError('wrong number of columns')
-#            for z in zip(idx[0], range(len(value))):
-#                for w in zip(idx[1], range(len(value[0]))):
-#                    m_idx = self._rows*z[0] + w[0]
-#                    x = value[z[1]][w[1]]
-#                    _trackcpp.c_array_set(self._array, m_idx, x)
-#
-#    def _check_matrix_indices(self, k):
-#        if not isinstance(k, tuple):
-#            raise TypeError('indices must be tuple')
-#        if not len(k) == 2:
-#            raise LengthError('two indices are needed')
