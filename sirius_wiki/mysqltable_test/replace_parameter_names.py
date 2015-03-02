e3f3538a4ceeab979b74b5b6b6cf854ea1141e80
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-03-02 13:09:42 -0300

    Complete parameter renaming script
    Name table still needs to be completed.

--- sirius_wiki/mysqltable_test/replace_parameter_names.py
+++ sirius_wiki/mysqltable_test/replace_parameter_names.py
@@ -9,14 +9,14 @@ import pywikibot
 import pywikibot.pagegenerators
 
 
-NS_PARAMETER = 104
-NS_TABLE = 118
+PARAMETER_NS = 104
+TABLE_NS = 118
 
-RE_PURE_LINK = '\[\[[^\]\{]*\]\]'
-RE_LINK_WITH_TEMPLATE = '\[\[[^\]]*\{.*\]\]'
-RE_TEMPLATE_VALUE = '\{\{Storage ring value[^\{]*\}\}'
-RE_TEMPLATE_UNITS = '\{\{Storage ring units[^\{]*\}\}'
-RE_TEMPLATE = '\{\{Storage ring[^\{]*\}\}'
+PARAMETER_NS_STR = 'Parameter:'
+
+RE_LINK_WITH_TEMPLATE = '\[\[[^\]]*\{[^\[]*\]\]'
+RE_LINK_WITHOUT_TEMPLATE = '\[\[[^\]\{]*\]\]'
+RE_TEMPLATE = '\{\{[^\{]*\}\}'
 
 MACHINES = {
     'Linac': 'LI',
@@ -35,36 +35,92 @@ for m in MACHINES.keys() + MACHINES.values():
         TEMPLATES.append(m + ' ' + s)
 
 TABLE = {
+    'Storage ring beam gamma factor': 'SI beam gamma factor',
     'Storage ring horizontal tune': 'SI optics tune horizontal',
-    'Storage ring vertical tune': 'SI optics tune vertical'
+    'Storage ring vertical tune': 'SI optics tune vertical',
+    'Storage ring number of long straight sections': 'SI lattice long straight section number',
 }
 
 
 def replace_parameters(text):
-    # Find all templates
-    templates = re.findall('\{\{[^\{]*\}\}', text)
+    text = replace_links_with_templates(text)
+    text = replace_links(text)
+    text = replace_templates(text)
 
-    for t in templates:
-        for T in TEMPLATES:
-            if t.find(T) > 0:
-                break
-        else:
+    return text
+
+
+def replace_links_with_templates(text):
+    links = re.findall(RE_LINK_WITH_TEMPLATE, text)
+    for link in links:
+        templates = re.findall(RE_TEMPLATE, link)
+        if not len(templates) == 1:
             continue
 
-        for s in SPECIALISATIONS:
-            if t.find(s) > 0:
-                repl = get_template_replacement(t, s)
-                break
-        else:
-            repl = get_template_replacement(t)
+        repl = get_template_replacement(templates[0])
+        if repl is not None:
+            text = text.replace(link, repl)
 
+    return text
+
+
+def replace_links(text):
+    links = re.findall(RE_LINK_WITHOUT_TEMPLATE, text)
+    for link in links:
+        repl = get_link_replacement(link)
+        if repl is not None:
+            text = text.replace(link, repl)
+
+    return text
+
+
+def replace_templates(text):
+    templates = re.findall(RE_TEMPLATE, text)
+    for t in templates:
+        repl = get_template_replacement(t)
         if repl is not None:
             text = text.replace(t, repl)
 
     return text
 
 
-def get_template_replacement(template, specialisation=None):
+def get_link_replacement(link):
+    link = strip_link_braces(link)
+    if not link.startswith(PARAMETER_NS_STR):
+        return None
+
+    parts = link.split('|')
+    name = parts[0].lstrip(PARAMETER_NS_STR)
+    if name not in TABLE:
+        return None
+
+    new_name = TABLE[name]
+    if len(parts) == 1:
+        return '[[' + PARAMETER_NS_STR + new_name + ']]'
+    elif len(parts) == 2:
+        return '[[' + PARAMETER_NS_STR + new_name + '|' + parts[1] + ']]'
+    else:
+        return None
+
+
+def get_template_replacement(template):
+    for T in TEMPLATES:
+        if template.find(T) > 0:
+            break
+    else:
+        return None
+
+    for s in SPECIALISATIONS:
+        if template.find(s) > 0:
+            repl = get_specialised_template_replacement(template, s)
+            break
+    else:
+        repl = get_specialised_template_replacement(template)
+
+    return repl
+
+
+def get_specialised_template_replacement(template, specialisation=None):
     t = strip_template_braces(template)
     s = t.split('|')
 
@@ -99,18 +155,21 @@ def strip_template_braces(template):
     return template.lstrip('{').rstrip('}')
 
 
+def strip_link_braces(link):
+    return link.lstrip('[').rstrip(']')
+
+
 if __name__ == '__main__':
     site = pywikibot.Site('en', 'siriuswiki')
     g = pywikibot.pagegenerators.AllpagesPageGenerator(
         site=site,
-        namespace=NS_TABLE
+        namespace=TABLE_NS
     )
 
     i = 0
     pages = []
     for page in g:
         pages.append(page)
-        # print(str(i) + ': ' + page.title())
         i += 1
 
     page = pages[45]
