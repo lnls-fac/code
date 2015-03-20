9ee93a9f41f1b9e398c7d07ad6ca395581477922
Fernando Henrique de Sá <fernando@lnls82-linux.abtlus.org.br>
2015-03-19 21:05:27 -0300

    Improvements in tracking velocity for rms machines
    Change in trackcpp_run_dynap_calc_random_machines.m to submit different jobs for daxy, daex and ma calcs.
    Change in tracy3_da_ma_lt.m to permit vizualization of daxy, daex and ma results independently.

--- MatlabMiddleLayer/Release/lnls/fac_scripts/sirius/nonlinear_beam_stay_clear/calc_nonlinear_bsc.m
+++ MatlabMiddleLayer/Release/lnls/fac_scripts/sirius/nonlinear_beam_stay_clear/calc_nonlinear_bsc.m
@@ -1,5 +1,5 @@
-function calc_nonlinear_bsc(the_ring, n_per, cam_vac)
-% calc_nonlinear_bsc(the_ring, nper, cam_vac)
+function calc_nonlinear_bsc(the_ring, n_per)
+% calc_nonlinear_bsc(the_ring, nper)
 %
 % calcula o beam stay clear nao linear para o primeiro superperiodo da rede
 % the_ring, que possui n_per periodos. cam_vac eh o valor do raio da camara
@@ -26,18 +26,6 @@ points = sort([points, points2]);
 %Define sposition of the calculated points
 spos = spos(points);
 
-
-%pedir confimacao de calculo, para dificultar possiveis absurdos
-resp = 'n';
-while ~strcmp(resp,'s')
-    prompt = sprintf('calcular o bsc nao linear para %d pontos da rede?(s/n): ', ...
-        length(points));
-    resp = input(prompt, 's');
-    if strcmp(resp,'n')
-        return
-    end
-end
-
 %agora, tenho que preparar os arquivos para o calculo
 %primeiro vamos definir as condicoes iniciais de varredura:
 np = 121;
@@ -45,42 +33,47 @@ np = 121;
 % e quantas voltas eu quero dar
 nturns = 500;
 
+cam_vac = getcellstruct(the_ring,'VChamber',1:length(the_ring));
+cam_vac = cell2mat(cam_vac)';
+cam_vac = cam_vac([1,2],:);
+cam_vacx = cam_vac(1,:);
+cam_vacy = cam_vac(2,:);
 % preciso desnormalizar pelo gammax, para que no ponto de beta maximo a
 % varredura va ate 12mm.
-twissdata = twissring(the_ring(1:points(end)),0,1:points(end)); 
+twissdata = twissring(the_ring,0,1:length(the_ring)); 
 spos_twi = cat(1,twissdata.SPos)';
-beta   = cat(1,twissdata.beta);     alpha  = cat(1,twissdata.alpha);
-betax  = beta(:,1)';                 betay  = beta(:,2)'; 
-alphax = alpha(:,1)';                alphay = alpha(:,2)';
-gammax = (1+alphax.^2)./betax;      gammay = (1+alphay.^2)./betay;
-[mingammax, ~] = min(gammax);    [mingammay, ~] = min(gammay);
-x0 = 1.2*linspace(-cam_vac,cam_vac,np)*sqrt(mingammax);
-y0 = 1.2*linspace(-cam_vac,cam_vac,np)*sqrt(mingammay);
+beta   = cat(1,twissdata.beta);  
+alpha  = cat(1,twissdata.alpha);
+betax  = beta(:,1)';                 
+betay  = beta(:,2)'; 
+alphax = alpha(:,1)';                
+alphay = alpha(:,2)';
+gammax = (1+alphax.^2)./betax;      
+gammay = (1+alphay.^2)./betay;
+[hor_accep, ~] = min(cam_vacx./sqrt(betax)); 
+[ver_accep, ~] = min(cam_vacy./sqrt(betay));
+x0 = 1.2*linspace(-1,1,np)*hor_accep;
+y0 = 1.2*linspace(-1,1,np)*ver_accep;
 
 %nao quero que as particulas andem exatamente na linha y=0 e x=0 para
 %as varreduras em x e y, respectivamente, entao vou gerar uma
 %distribuicao gaussiana sigma 0.1mm com corte em um sigma:
-y0_rand = get_random_numbers(0.1e-3, np, 1)'*sqrt(mingammay);
-x0_rand = get_random_numbers(0.1e-3, np, 1)'*sqrt(mingammax);
-
+x0_rand = get_random_numbers(1e-2, np, 1)'*hor_accep;
+y0_rand = get_random_numbers(1e-2, np, 1)'*ver_accep;
 
 bsch_pos = zeros(1,length(points));
 bsch_neg = zeros(1,length(points));
 bscv_pos = zeros(1,length(points));
 bscv_neg = zeros(1,length(points));
-fprintf('\n\nOlha, nao bota toda sua feh no que eu vou falar agora nao.\n');
-fprintf('Se for pra apostar...\n');
-sleep(5);
-fprintf('Serio, nao aposte!\n');
-fprintf('Mas eu acho que pra terminar, como ja foram\n');
+fprintf('Eu acho que pra terminar, como ja foram\n');
 for ii=1:length(points)
     %estimativa de tempo de calculo
-    fprintf('%2d calculos de %2d, ainda faltam %5f minutos\n',ii-1,...
+    fprintf('%2d calculos de %2d, ainda faltam %5.2f minutos\n',ii-1,...
         length(points),15/100*nturns/60*(length(points)-ii+1));
     
     % primeiro vamos shiftar a rede para comecar no ponto de calculo
-    ind = [points(ii):(length(the_ring)-1) 2:(points(ii)-1)];
-    the_ring_calc = the_ring([1 ind end]);
+    ind1 = [points(ii):(length(the_ring)-1) 2:(points(ii)-1)];
+    the_ring_calc = the_ring([1,ind1,end]);
     
     %agora, vamos reescalar a varredura, para ter a mesma precisao:
     x0_rees = x0/sqrt(gammax(points(ii)));
@@ -91,12 +84,12 @@ for ii=1:length(points)
     % agora eu monto as condicoes iniciais
     Rin = [[x0_rees; zeros(1,np);y_rand;zeros(3,np)] [x_rand; zeros(1,np);y0_rees;zeros(3,np)]];
     
-    
     %primeiro eu faco um tracking detalhado para ver se as particulas estao
     %furando a camara de vacuo
     Rout=Rin;
     len_ring = length(the_ring_calc);
     nt_det =60; %tempo para elas darem mais de 3 voltas no espaco de fase
+    np_left = 2*np;
     if 3*nt_det >= nturns
         error('Coloque mais voltas em nturns que 3*nt_det');
     end
@@ -104,26 +97,27 @@ for ii=1:length(points)
         %track
         Rout = linepass(the_ring_calc,Rout,1:len_ring);
         % look to x and y along all ring if it hit the vac_cham
-        ind = (abs(Rout([1 3],:)) >= cam_vac);
+        cam = [reshape(repmat(cam_vacx([1,ind1,end]),np_left,1),1,[]);...
+               reshape(repmat(cam_vacy([1,ind1,end]),np_left,1),1,[])];
+        ind = (abs(Rout([1 3],:)) >= cam);
         %if x or y hit
         ind = any(ind);
         % at any point of the ring
-        ind = any(reshape(ind,length(ind)/len_ring,len_ring)');
+        ind = any(reshape(ind,[],len_ring)');
         % or if the particle is lost by other means
         Rout = Rout(:,(end-length(ind)+1):end);
         ind = ind | isnan(Rout(1,:));
         %I exclude it from the next tracking
         Rout = Rout(:,~ind);
+        np_left = size(Rout,2);
     end
-    %quantas particulas nao foram perdidas
-    part_left = size(Rout,2);
     %faz o tracking de varias voltas no anel
     Rout = ringpass(the_ring_calc,Rout,nturns-nt_det);
     
     %agora eu vejo se ele passou a camara de vacuo naquele ponto
-    ind = (abs(Rout([1 3],:)) >= cam_vac);
+    ind = (abs(Rout([1 3],:)) >= repmat(cam_vac(:,points(ii)),1,(nturns-nt_det)*np_left));
     ind = any(ind); %se passou em x ou y
-    ind = reshape(ind,part_left,nturns-nt_det)'; %em alguma das voltas
+    ind = reshape(ind,np_left,nturns-nt_det)'; %em alguma das voltas
     ind = any(ind);
     Rout = Rout(:,repmat(~ind,1,nturns-nt_det)); %excluo do vetor saida
     
@@ -131,20 +125,7 @@ for ii=1:length(points)
     bsch_pos(ii) = max(Rout(1,:));
     bsch_neg(ii) = min(Rout(1,:));
     bscv_pos(ii) = max(Rout(3,:));
-    bscv_neg(ii) = min(Rout(3,:));
-    
-    %quero plotar o espaco de fase no mc para mostrar a varredura
-    if strcmp(the_ring_calc{2}.FamName,'mc');
-        part_left = size(Rout,2)/(nturns-nt_det);
-        x = reshape(Rout(1,:),part_left,nturns-nt_det)';
-        xp = reshape(Rout(2,:),part_left,nturns-nt_det)';
-        figure; plot(x,xp,'Marker','.','LineStyle','none'); xlabel('x');ylabel('xp');
-        y = reshape(Rout(3,:),part_left,nturns-nt_det)';
-        yp = reshape(Rout(4,:),part_left,nturns-nt_det)';
-        figure; plot(y,yp,'Marker','.','LineStyle','none'); xlabel('y');ylabel('yp');
-        figure; plot(x,y,'Marker','.','LineStyle','none'); xlabel('x');ylabel('y');
-        drawnow;
-    end   
+    bscv_neg(ii) = min(Rout(3,:));  
 end
 
 % vamos salvar os dados do BSC nao linear:
@@ -161,42 +142,32 @@ fclose(fp);
 
 
 % vamos calcular o bsc linear para comparar os resultados:
-bschl = cam_vac * sqrt(betax/max(betax));
-bscvl = cam_vac * sqrt(betay/max(betay));
-
-
-% também, vamos calcular o não linear, escalado linearmente
-[betamax ind] = max(betax(points));
-bschnl_pos    = bsch_pos(ind)*sqrt(betax/betamax);
-bschnl_neg    = bsch_neg(ind)*sqrt(betax/betamax);
-[betamay ind] = max(betay(points));
-bscvnl_pos    = bscv_pos(ind)*sqrt(betay/betamay);
-bscvnl_neg    = bscv_neg(ind)*sqrt(betay/betamay);
-
+bschl = hor_accep * sqrt(betax);
+bscvl = ver_accep * sqrt(betay);
 
 %plota os resultados
-fig = figure('OuterPosition',get(0,'screenSize'));
+fig = figure('Position',[1,1,700,434]);
 
  axes_bscv = subplot(2,1,1,'Parent',fig,'YGrid','on','FontSize',16);
 box(axes_bscv,'on');
 hold on
-plot1(:,1) = plot(spos,[bscv_pos;bscv_neg]*1e3,'MarkerSize',10,'Marker','.','Color','b');
-plot1(:,2) = plot(spos_twi,[bscvnl_pos;bscvnl_neg]*1e3,'Color','g');
-plot1(:,3) = plot(spos_twi,[bscvl;-bscvl]*1e3,'Color','r');
-drawlattice(0,1,axes_bscv,spos(end));
+plot1(:,1) = plot(spos,[bscv_pos;bscv_neg]*1e3,'MarkerSize',10,'Marker','.','Color','b','LineWidth',2);
+plot1(:,2) = plot(spos_twi,[bscvl;-bscvl]*1e3,'Color','r','LineWidth',1);
+plot1(:,3) = plot(spos_twi,[-cam_vacy;cam_vacy]*1e3,'Color','k','LineWidth',2);
+lnls_drawlattice(the_ring,n_per,0,true,1,true,axes_bscv);
 xlabel('Position [m]');
 xlim([spos(1) spos(end)]);
 ylabel('Vertical Beam Stay Clear [mm]');
-legend(plot1(1,:),'show',{'non-linear';'misto';'linear'});
+legend(plot1(1,:),'show',{'non-linear';'linear';'vaccum_chamber'});
 
 
 axes_bsch = subplot(2,1,2,'Parent',fig,'YGrid','on','FontSize',16);
 box(axes_bsch,'on');
 hold on
-plot(spos,[bsch_pos;bsch_neg]*1e3,'MarkerSize',10,'Marker','.','Color','b');
-plot(spos_twi,[bschnl_pos;bschnl_neg]*1e3,'Color','g');
-plot(spos_twi,[bschl;-bschl]*1e3,'Color','r');
-drawlattice(0,1,axes_bsch,spos(end));
+plot(spos,[bsch_pos;bsch_neg]*1e3,'MarkerSize',10,'Marker','.','Color','b','LineWidth',1);
+plot(spos_twi,[bschl;-bschl]*1e3,'Color','r','LineWidth',1);
+plot(spos_twi,[-cam_vacx;cam_vacx]*1e3,'Color','k','LineWidth',2);
+lnls_drawlattice(the_ring,n_per,0,true,1,true,axes_bsch);
 xlabel('Position [m]');
 xlim([spos(1) spos(end)]);
 ylabel('Horizontal Beam Stay Clear [mm]');
