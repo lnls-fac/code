8312d9c9ef93a54d4e8c98be7a3e6159f891dd50
ximenes <xresende@gmail.com>
2015-04-04 21:37:23 -0300

    lnls and sirius
    - added 'lnls' package to this branch
    - fixed tests.py in 'sirius'

--- sirius/_system.py
+++
@@ -1,3 +0,0 @@
-folder_root = '/home/fac_files'
-folder_data = folder_root + '/' + 'data/'
-folder_code = folder_root + '/' + 'code/'
--- sirius/tests.py
+++ sirius/tests.py
@@ -7,29 +7,28 @@ import matplotlib.pyplot as plt
 # creates accelerate and inits its lattice
 the_ring = sirius.create_accelerator()
 
-accelerator.lattice = pyaccel.lattice.Lattice(elements=sirius.create_lattice())
-#
-# # global tracking parameters
-# accelerator.cavity_on = False
-# accelerator.radiation_on = False
-# accelerator.vchamber_on = False
+# global tracking parameters
+the_ring.cavity_on = True
+the_ring.radiation_on = True
+the_ring.vchamber_on = False
 
-# # aux. parameters and symbols
-# the_ring = accelerator.lattice
-# num_turns, trajectory, offset = 512, True, 0
+# aux. parameters and symbols
+# the_ring = the_ring.lattice
+num_turns, trajectory, offset = 4*512, True, 0
 #
 # # calcs phase space at MIA
-# pos = [0.001,0,0,0,0,0]
-# traj, turn, offset, plane = pyaccel.tracking.ringpass(accelerator, pos, num_turns, trajectory, offset)
-# plt.plot(traj[0,:],traj[1,:],'o')
-#
-# # propagates from MIA to MIB
-# traj, offset, plane = pyaccel.tracking.linepass(accelerator, pos, trajectory, offset)
-# idx = pyaccel.lattice.findcells(the_ring, 'fam_name', 'mib')
-#
-# # calcs phase space at MIB
-# pos = traj[:,idx[0]]
-# traj, turn, offset, plane = pyaccel.tracking.ringpass(accelerator, pos, num_turns, trajectory, offset=idx[0])
-# plt.plot(traj[0,:],traj[1,:],'o')
-#
-# plt.show()
+pos = [0.001,0,0,0,0,0]
+traj, turn, offset, plane = pyaccel.tracking.ringpass(the_ring, pos, num_turns, trajectory, offset)
+plt.plot(1000*traj[0,:],1000*traj[1,:],'o')
+plt.xlabel('x[mm]'), plt.ylabel("x'[mrad]")
+# propagates from MIA to MIB
+traj, offset, plane = pyaccel.tracking.linepass(the_ring, pos, trajectory, offset)
+idx = pyaccel.lattice.findcells(the_ring, 'fam_name', 'mib')
+
+# calcs phase space at MIB
+pos = traj[:,idx[0]]
+traj, turn, offset, plane = pyaccel.tracking.ringpass(the_ring, pos, num_turns, trajectory, offset=idx[0])
+plt.plot(1000*traj[0,:],1000*traj[1,:],'o')
+plt.xlabel('x[mm]'), plt.ylabel("x'[mrad]")
+
+plt.show()
