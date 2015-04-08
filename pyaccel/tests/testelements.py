87d754f2ce2b7396f52a56316510267d9f1e277d
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-04-08 18:19:07 -0300

    Add more tests to elements

--- pyaccel/tests/testelements.py
+++ pyaccel/tests/testelements.py
@@ -87,19 +87,129 @@ class TestElement(unittest.TestCase):
 
         # Set single element
         self.element.r_in[2, 5] = -10.0
-        self.assertAlmostEqual(self.element.t_in[2, 5], -10.0)
+        self.assertAlmostEqual(self.element.r_in[2, 5], -10.0)
 
-        # # Set list of indices
-        # self.element.t_out[[1, 3, 5]] = [1.0, 3.0, 5.0]
-        # self.assertAlmostEqual(self.element.t_out[1], 1.0)
-        # self.assertAlmostEqual(self.element.t_out[3], 3.0)
-        # self.assertAlmostEqual(self.element.t_out[5], 5.0)
-        #
-        # # Set slice
-        # self.element.t_out[3:] = [-1.0, -2.0, -3.0]
-        # self.assertAlmostEqual(self.element.t_out[3], -1.0)
-        # self.assertAlmostEqual(self.element.t_out[4], -2.0)
-        # self.assertAlmostEqual(self.element.t_out[5], -3.0)
+        # Set list of indices
+        self.element.r_out[[2, 3, 4], [1, 3, 5]] = [1.0, 3.0, 5.0]
+        self.assertAlmostEqual(self.element.r_out[2, 1], 1.0)
+        self.assertAlmostEqual(self.element.r_out[3, 3], 3.0)
+        self.assertAlmostEqual(self.element.r_out[4, 5], 5.0)
+
+        # Set slice
+        self.element.r_out[1, 3:] = [-1.0, -2.0, -3.0]
+        self.assertAlmostEqual(self.element.r_out[1, 3], -1.0)
+        self.assertAlmostEqual(self.element.r_out[1, 4], -2.0)
+        self.assertAlmostEqual(self.element.r_out[1, 5], -3.0)
+
+    def test_set_pass_method_from_index(self):
+        for i in range(len(pyaccel.elements.pass_methods)):
+            pass_method = pyaccel.elements.pass_methods[i]
+            self.element.pass_method = i
+            self.assertEqual(self.element.pass_method, pass_method)
+
+    def test_set_pass_method_from_string(self):
+        for pass_method in pyaccel.elements.pass_methods:
+            self.element.pass_method = pass_method
+            self.assertEqual(self.element.pass_method, pass_method)
+
+    def test_set_invalid_pass_method_from_index(self):
+        error = False
+        try:
+            self.element.pass_method = len(pyaccel.elements.pass_methods)
+        except:
+            error = True
+        self.assertTrue(error, "invalid pass method set")
+
+    def test_set_invalid_pass_method_from_string(self):
+        error = False
+        try:
+            self.element.pass_method = 'invalid_pass_method'
+        except:
+            error = True
+        self.assertTrue(error, "invalid pass method set")
+
+
+class TestTrackCppElement(unittest.TestCase):
+
+    def setUp(self):
+        self.element = pyaccel.elements.Element()
+        self.trackcpp_element = self.element._e
+
+    def test_pass_method(self):
+        index = 2
+        self.element.pass_method = pyaccel.elements.pass_methods[index]
+        self.assertEqual(self.trackcpp_element.pass_method, index)
+
+    def test_fam_name(self):
+        fam_name = 'FAM'
+        self.element.fam_name = fam_name
+        self.assertEqual(self.trackcpp_element.fam_name, fam_name)
+
+    def test_angle(self):
+        angle = 0.123456789
+        self.element.angle = angle
+        self.assertAlmostEqual(self.trackcpp_element.angle, angle)
+
+    def test_t_in(self):
+        value = -1.2345
+        self.element.t_in[2] = value
+        cpp_value = trackcpp.c_array_get(self.trackcpp_element.t_in, 2)
+        self.assertAlmostEqual(cpp_value, value)
+
+    def test_r_out(self):
+        value = -1.2345
+        self.element.r_out[2, 5] = value
+        cpp_value = trackcpp.c_array_get(self.trackcpp_element.r_out, 2*6 + 5)
+        self.assertAlmostEqual(cpp_value, value)
+
+
+class TestCreationFunctions(unittest.TestCase):
+
+    def test_marker(self):
+        name = 'Marker'
+        m = pyaccel.elements.marker(name)
+        self.assertEqual(m.fam_name, name)
+
+    def test_bpm(self):
+        name = 'BPM'
+        b = pyaccel.elements.bpm(name)
+        self.assertEqual(b.fam_name, name)
+
+    def test_drift(self):
+        name = 'Drift'
+        length = 1.2345
+        d = pyaccel.elements.drift(name, length)
+        self.assertEqual(d.fam_name, name)
+        self.assertAlmostEqual(d.length, length)
+
+    def test_hcorrector(self):
+        name = 'CH'
+        length = 1.2345
+        kick = 0.4321
+        c = pyaccel.elements.hcorrector(name, length, kick)
+        self.assertEqual(c.fam_name, name)
+        self.assertAlmostEqual(c.length, length)
+        self.assertAlmostEqual(c.hkick, kick)
+
+    def test_vcorrector(self):
+        name = 'CV'
+        length = 1.2345
+        kick = 0.4321
+        c = pyaccel.elements.vcorrector(name, length, kick)
+        self.assertEqual(c.fam_name, name)
+        self.assertAlmostEqual(c.length, length)
+        self.assertAlmostEqual(c.vkick, kick)
+
+    def test_corrector(self):
+        name = 'Corrector'
+        length = 1.2345
+        hkick = 0.4321
+        vkick = -0.1234
+        c = pyaccel.elements.corrector(name, length, hkick, vkick)
+        self.assertEqual(c.fam_name, name)
+        self.assertAlmostEqual(c.length, length)
+        self.assertAlmostEqual(c.hkick, hkick)
+        self.assertAlmostEqual(c.vkick, vkick)
 
 
 def element_suite():
@@ -107,7 +217,19 @@ def element_suite():
     return suite
 
 
+def trackcpp_element_suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTrackCppElement)
+    return suite
+
+
+def creation_functions_suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCreationFunctions)
+    return suite
+
+
 def get_suite():
     suite_list = []
     suite_list.append(element_suite())
+    suite_list.append(trackcpp_element_suite())
+    suite_list.append(creation_functions_suite())
     return unittest.TestSuite(suite_list)
