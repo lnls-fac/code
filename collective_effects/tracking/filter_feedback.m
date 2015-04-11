7c871762eee4746b5186d6832e398101d924cab1
Fernando Henrique de Sá <fernando@lnls210-linux.abtlus.org.br>
2015-04-10 21:36:03 -0300

    Impedance Tracking update. Increase in velocity of tracking with resonator.

--- collective_effects/tracking/filter_feedback.m
+++ collective_effects/tracking/filter_feedback.m
@@ -8,12 +8,12 @@ samp = 1:len;
 
 fil = cos(2*pi*freq*samp + phase).*sin(2*pi*samp/(2*len))./(samp*pi);
 
-% n = (0:199)/400;
-% x = cos(2*pi*n'*samp);
+n = (0:199)/400;
+x = cos(2*pi*n'*samp);
+
+figure; plot(n,20*log(abs(fil*x')))
+
+% phi = (0:199)/200 * 2 * pi;
+% y = cos(bsxfun(@plus,2*pi*0.12*samp',phi));
 % 
-% figure; plot(n,20*log(abs(fil*x')))
-
-phi = (0:199)/200 * 2 * pi;
-y = cos(bsxfun(@plus,2*pi*0.12*samp',phi));
-
-figure; plot(y(end,:), fil*y);
+% figure; plot(y(end,:), fil*y);
